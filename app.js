112db42c3c375b4c3941c78c80a52342e7b2c525
Anthony Eriksen <ajrayid@gmail.com>
2020-03-26 14:16:35 -0600

    Adding some styling

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+const path = require('path');
+
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}))
+  })
+);
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
-})
+  res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
+});
 
 app.listen(3000);
