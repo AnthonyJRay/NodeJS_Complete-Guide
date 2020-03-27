9587a78e9e4f6334fc42018b05677bce6a80f0e5
Anthony Eriksen <ajrayid@gmail.com>
2020-03-26 22:16:35 -0600

    Templating Engines: Pug, Handlebars, EJS

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(
@@ -14,11 +17,13 @@ app.use(
   })
 );
 
-app.use('/admin', adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', 'pageNotFound.html'));
+  res.status(404).render('pageNotFound', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
